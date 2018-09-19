78091b4620548e054291b53dcd0549f5a10bf810
zbw <15142354016@163.om>
2018-09-19 10:10:00 +0800

    v1

--- dgjj-website/src/main/java/com/dgjj/cloud/web/controller/DgjjTechProjectDriveController.java
+++ dgjj-website/src/main/java/com/dgjj/cloud/web/controller/DgjjTechProjectDriveController.java
@@ -1,11 +1,18 @@
 package com.dgjj.cloud.web.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dgjj.cloud.web.dto.DgjjMarketDto;
+import com.dgjj.cloud.web.form.DgjjMarketForm;
+import com.dgjj.cloud.web.service.DgjjMarketService;
 /**
  * 大国技交技术驱动
  *
@@ -15,12 +22,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DgjjTechProjectDriveController {
 	
+	@Autowired
+	DgjjMarketService dgjjMarketService;
+	
 	/**
 	 * 首页
 	 */
     @RequestMapping("/technology")
     public String technology(HttpServletRequest request, HttpServletResponse response, Model model){
 
+    	DgjjMarketForm form = new DgjjMarketForm();
+		List<DgjjMarketDto> list = dgjjMarketService.findList(form);
+		model.addAttribute("list",list);
+    		
         return "technology";
     }
 }
